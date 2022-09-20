8ee7d6f7506b3a302fa4c6cbd8e2897f8c5125c3
hellobrother1472 <paras1472@gmail.com>
2022-09-21 00:19:53 +0530

    new

--- All Nodes Distance K in Binary Tree.cpp
+++ All Nodes Distance K in Binary Tree.cpp
@@ -7,221 +7,237 @@
 
 using namespace std;
 
-struct TreeNode {
+struct TreeNode
+{
     int val;
     TreeNode *left;
     TreeNode *right;
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-
-    void assignParent(map<TreeNode*,TreeNode*>&m,TreeNode*root){
-        if(root == NULL || (root->left == NULL && root->right == NULL)){
+    void assignParent(map<TreeNode *, TreeNode *> &m, TreeNode *root)
+    {
+        if (root == NULL || (root->left == NULL && root->right == NULL))
+        {
             return;
         }
-        if(root->left){
+        if (root->left)
+        {
             m[root->left] = root;
-        } 
-        if(root->right){
+        }
+        if (root->right)
+        {
             m[root->right] = root;
-        } 
-        assignParent(m,root->left);
-        assignParent(m,root->right);
+        }
+        assignParent(m, root->left);
+        assignParent(m, root->right);
     }
-    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        vector<int>ans;
-        map<TreeNode*,TreeNode*> m;
+    vector<int> distanceK(TreeNode *root, TreeNode *target, int k)
+    {
+        vector<int> ans;
+        map<TreeNode *, TreeNode *> m;
         // Assigning the parents to the node.
-        assignParent(m,root);
-        
+        assignParent(m, root);
+
         // taking the radial step in all direction one step at a time and visited array is taking care of what node we already visited.
         int distance = 0;
-        queue<TreeNode*>q;
-        vector<int>visited;
+        queue<TreeNode *> q;
+        vector<int> visited;
         q.push(target);
-        while(distance < k){
+        while (distance < k)
+        {
             int size = q.size();
             for (int i = 0; i < size; i++)
             {
-                TreeNode*curr = q.front();
+                TreeNode *curr = q.front();
                 visited.push_back(curr->val);
                 q.pop();
                 // Left
-                if(curr->left && !count(visited.begin(),visited.end(),curr->left->val)){
+                if (curr->left && !count(visited.begin(), visited.end(), curr->left->val))
+                {
                     q.push(curr->left);
-                }      
-                // Right   
-                if(curr->right && !count(visited.begin(),visited.end(),curr->right->val)){
+                }
+                // Right
+                if (curr->right && !count(visited.begin(), visited.end(), curr->right->val))
+                {
                     q.push(curr->right);
                 }
-                // top 
-                if(m[curr] && !count(visited.begin(),visited.end(),m[curr]->val)){
+                // top
+                if (m[curr] && !count(visited.begin(), visited.end(), m[curr]->val))
+                {
                     q.push(m[curr]);
-                }                
+                }
             }
-            
+
             distance++;
-            if(distance == k){
-                break;
-            }
-        }
-        while(!q.empty()){
-            ans.push_back(q.front()->val);
-            q.pop();
-        }
-        return ans;
-    }
-};  
-
-// Using map instead of visited vector
-class Solution {
-public:
-
-    void assignParent(map<TreeNode*,TreeNode*>&m,TreeNode*root){
-        if(root == NULL || (root->left == NULL && root->right == NULL)){
-            return;
-        }
-        if(root->left){
-            m[root->left] = root;
-        } 
-        if(root->right){
-            m[root->right] = root;
-        } 
-        assignParent(m,root->left);
-        assignParent(m,root->right);
-    }
-    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        vector<int>ans;
-        map<TreeNode*,TreeNode*> m;
-        // Assigning the parents to the node.
-        assignParent(m,root);
-        
-        // taking the radial step in all direction one step at a time and visited array is taking care of what node we already visited.
-        int distance = 0;
-        queue<TreeNode*>q;
-        // vector<int>visited;
-        map<TreeNode*,bool>visited;
-        q.push(target);
-        while(distance < k){
-            int size = q.size();
-            for (int i = 0; i < size; i++)
+            if (distance == k)
             {
-                TreeNode*curr = q.front();
-                visited[curr] = true;
-                q.pop();
-                // Left
-                if(curr->left && !visited[curr->left]){
-                    q.push(curr->left);
-                }      
-                // Right   
-                if(curr->right && !visited[curr->right]){
-                    q.push(curr->right);
-                }
-                // top 
-                if(m[curr] && !visited[m[curr]]){
-                    q.push(m[curr]);
-                }                
-            }
-            
-            distance++;
-            if(distance == k){
                 break;
             }
         }
-        while(!q.empty()){
+        while (!q.empty())
+        {
             ans.push_back(q.front()->val);
             q.pop();
         }
         return ans;
-    }
-};  
-
-// Reccursive Solution
-class Solution {
-public:
-    
-    vector<TreeNode*>nodePath(TreeNode* root,TreeNode*target){
-        
-        if(root == NULL)
-            return {};
-    
-        if(root->val ==target->val)
-            return {root};
-        
-        vector<TreeNode*>ans;
-        
-        ans = nodePath(root->left,target);
-        
-        if(ans.size()>0){
-            ans.push_back(root);
-            return ans ;
-        }
-        
-        ans = nodePath(root->right,target);
-        
-        if(ans.size()>0){
-            ans.push_back(root);
-            return ans;
-        }
-        
-        return {};
-        
-    }
-    
-    vector<int>ans;
-    
-    void findDown(TreeNode* root,int k,int level){
-        
-        if(root == NULL)
-            return;
-        
-        if(k==level){
-            ans.push_back(root->val);
-            return;
-        }
-        
-        findDown(root->left,k,level+1);
-        findDown(root->right,k,level+1);
-        
-    }
-    
-    void findNodes(TreeNode* root,int level,int k, TreeNode* danger){
-        
-        if(root == NULL)
-            return ;
-        
-        if(root == danger)
-            return ;
-        
-        if(level == k){
-            ans.push_back(root->val);
-            return;
-        }
-        
-        findNodes(root->left,level+1,k,danger);
-        findNodes(root->right,level+1,k,danger);
-        
-    }
-    
-    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        
-        vector<TreeNode*>path = nodePath(root,target);
-        
-        findDown(path[0],k,0);
-        
-        for(int ctr = 1 ; ctr < path.size();ctr++){
-            findNodes(path[ctr],0,k-ctr,path[ctr-1]);
-        }
-        
-        return ans ;
-        
     }
 };
 
-int main(){
-    
+// Using map instead of visited vector
+class Solution
+{
+public:
+    void assignParent(map<TreeNode *, TreeNode *> &m, TreeNode *root)
+    {
+        if (root == NULL || (root->left == NULL && root->right == NULL))
+        {
+            return;
+        }
+        if (root->left)
+        {
+            m[root->left] = root;
+        }
+        if (root->right)
+        {
+            m[root->right] = root;
+        }
+        assignParent(m, root->left);
+        assignParent(m, root->right);
+    }
+    vector<int> distanceK(TreeNode *root, TreeNode *target, int k)
+    {
+        vector<int> ans;
+        map<TreeNode *, TreeNode *> m;
+        // Assigning the parents to the node.
+        assignParent(m, root);
+
+        // taking the radial step in all direction one step at a time and visited array is taking care of what node we already visited.
+        int distance = 0;
+        queue<TreeNode *> q;
+        // vector<int>visited;
+        map<TreeNode *, bool> visited;
+        q.push(target);
+        while (distance < k)
+        {
+            int size = q.size();
+            for (int i = 0; i < size; i++)
+            {
+                TreeNode *curr = q.front();
+                visited[curr] = true;
+                q.pop();
+                // Left
+                if (curr->left && !visited[curr->left])
+                {
+                    q.push(curr->left);
+                }
+                // Right
+                if (curr->right && !visited[curr->right])
+                {
+                    q.push(curr->right);
+                }
+                // top
+                if (m[curr] && !visited[m[curr]])
+                {
+                    q.push(m[curr]);
+                }
+            }
+
+            distance++;
+            if (distance == k)
+            {
+                break;
+            }
+        }
+        while (!q.empty())
+        {
+            ans.push_back(q.front()->val);
+            q.pop();
+        }
+        return ans;
+    }
+};
+
+// Reccursive Solution
+// Explanation link - https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/discuss/1126885/Recursion-time%3A-O(N)-or-memory%3A-O(N)
+
+// Basically we are finding the root and returning 1 if we got the root. When we got the root we perform operation to get all value below it and return 1 (or distance from the target) and if parent get 1(or distance from the target)  from any child the it will perform operation to get elements at opposite node with desired distance and returns the distance+1 to it's parent (which is distance of parent from the target).
+class Solution
+{
+public:
+    std::vector<int> distanceK(TreeNode *root, TreeNode *target, int K)
+    {
+        std::vector<int> res;
+
+        distanceK(root, target, K, res);
+
+        return res;
+    }
+
+private:
+    int distanceK(TreeNode *node, TreeNode *target, int K, std::vector<int> &res)
+    {
+        if (node == nullptr)
+        {
+            return 0;
+        }
+        if (node == target)
+        { // found the target for the current subtree
+            traverseSubtree(node, K, res);
+            return 1;
+        }
+        int distLeft = distanceK(node->left, target, K, res); // distance to target in the left subtree
+
+        if (distLeft != 0)
+        {
+            if (distLeft == K)
+            {
+                res.push_back(node->val);
+            }
+            else if (distLeft < K)
+            {
+                traverseSubtree(node->right, K - distLeft - 1, res); // visit the right subtree looking for nodes at a given distance
+            }
+            return distLeft + 1; //  return distance to target + 1
+        }
+        int distRight = distanceK(node->right, target, K, res); // distance to target in the right subtree
+
+        if (distRight != 0)
+        {
+            if (distRight == K)
+            {
+                res.push_back(node->val);
+            }
+            else if (distRight < K)
+            {
+                traverseSubtree(node->left, K - distRight - 1, res); // visit the left subtree looking for nodes at a given distance
+            }
+            return distRight + 1; //  return distance to target + 1
+        }
+        return 0; // if reached this place, then there is no target in the current subtree
+    }
+
+    void traverseSubtree(TreeNode *node, int K, std::vector<int> &res)
+    {
+        if (node == nullptr)
+        {
+            return;
+        }
+        if (K == 0)
+        { // found a node at a given distance
+            res.push_back(node->val);
+            return;
+        }
+        traverseSubtree(node->left, K - 1, res);  // go down until we find a node at a distance of K
+        traverseSubtree(node->right, K - 1, res); // (if the depth of the subtree allows us to do this)
+    }
+};
+
+int main()
+{
 
     return 0;
 }
