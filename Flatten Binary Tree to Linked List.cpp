9dfb9e66e161a5a29bddb0df0ef60fc7a63cc69c
hellobrother1472 <paras1472@gmail.com>
2022-09-27 21:34:59 +0530

    new

--- Flatten Binary Tree to Linked List.cpp
+++ Flatten Binary Tree to Linked List.cpp
@@ -94,6 +94,30 @@ public:
     }
 };
 
+// Using morris traversal
+class Solution4
+{
+public:
+    TreeNode*prev = NULL;
+    void flatten(TreeNode *root)
+    {
+        TreeNode*curr = root;
+        while(curr != NULL){
+            if(curr->left){
+                TreeNode*prev = curr->left;
+                while(prev->right){
+                    prev = prev->right;
+                }
+
+                prev->right = curr->right;
+                curr->right = curr->left;
+                curr->left = NULL;
+            }
+            curr = curr->right;
+        }
+    }
+};
+
 
 int main()
 {
