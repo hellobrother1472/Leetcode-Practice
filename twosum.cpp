9e29993068e200920bf5a3197e73dd701dc4e89a
hellobrother1472 <paras1472@gmail.com>
2022-10-15 00:31:46 +0530

    new

--- twosum.cpp
+++ twosum.cpp
@@ -35,6 +35,47 @@ public:
     }
 };
 
+// Using two pointer
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res,store;
+    	store = nums;
+
+    	sort(store.begin(), store.end());
+
+    	int left=0,right=nums.size()-1;
+    	int n1,n2;
+
+    	while(left<right){
+        	if(store[left]+store[right]==target){
+
+            	n1 = store[left];
+            	n2 = store[right];
+
+            	break;
+
+        	}
+        	else if(store[left]+store[right]>target)
+            	    right--;
+        	else
+            	    left++;
+    	}
+
+        // Getting the index of the founded elements
+    	for(int i=0;i<nums.size();++i){
+
+        	if(nums[i]==n1)
+            	    res.emplace_back(i);
+        	else if(nums[i]==n2)
+            	    res.emplace_back(i);
+    	}
+
+    	    return res;
+    }
+    
+};
+
 void display(vector<int> a)
 {
     for (int i = 0; i < a.size(); i++)
