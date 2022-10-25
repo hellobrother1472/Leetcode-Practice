548f02e10eb707627ea472220afd1d105006df00
hellobrother1472 <paras1472@gmail.com>
2022-10-26 00:38:58 +0530

    new

--- Spiral Matrix.cpp
+++ Spiral Matrix.cpp
@@ -2,37 +2,52 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int>ans;
-    void solve(int start,int end,int height,vector<vector<int>>matrix){
+    vector<int> ans;
+    void solve(int start, int end, int height, vector<vector<int>> matrix)
+    {
         for (int i = start; i <= end; i++)
         {
             ans.push_back(matrix[start][i]);
         }
-        for (int i = start+1; i <= height; i++)
+        if (start != height)
         {
-            ans.push_back(matrix[i][end]);
+            for (int i = start + 1; i <= height; i++)
+            {
+                ans.push_back(matrix[i][end]);
+            }
+            for (int i = end - 1; i >= start; i--)
+            {
+                ans.push_back(matrix[height][i]);
+            }
+            if (start != end)
+            {
+                for (int i = height - 1; i >= start + 1; i--)
+                {
+                    ans.push_back(matrix[i][start]);
+                }
+            }
         }
-        for (int i = end-1; i >= start; i--)
-        {
-            ans.push_back(matrix[height][i]);
-        }
-        for (int i = height-1; i >= start+1; i--)
-        {
-            ans.push_back(matrix[i][start]);
-        }
-        
     }
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+    vector<int> spiralOrder(vector<vector<int>> &matrix)
+    {
         int start = 0;
-        int end = matrix[0].size()-1;
-        int height = matrix.size()-1;
+        int end = matrix[0].size() - 1;
+        int height = matrix.size() - 1;
+        while (start <= height && start <= end)
+        {
+            solve(start, end, height, matrix);
+            height--;
+            end--;
+            start++;
+        }
         return ans;
     }
 };
-
-int main(){   
+int main()
+{
 
     return 0;
 }
