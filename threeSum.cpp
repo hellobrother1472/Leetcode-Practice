695a092fba029d97e326d842f14f13ffa16cc807
hellobrother1472 <paras1472@gmail.com>
2022-11-03 00:15:36 +0530

    new

--- threeSum.cpp
+++ threeSum.cpp
@@ -2,41 +2,50 @@
 
 using namespace std;
 
-class Solution {
+// Algo is to take one constant and apply two sum on the rest.
+class Solution
+{
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>ans;
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        vector<vector<int>> ans;
         int n = nums.size();
-        sort(nums.begin(),nums.end());
-        int low,high;
-        for (int i = 0; i < n; i++)
+        sort(nums.begin(), nums.end());
+        int low, high;
+        for (int i = 0; i < n - 2; i++)
         {
-            if(i!=0 && nums[i] == nums[i-1]){
+            if (i != 0 && nums[i] == nums[i - 1])
                 continue;
-            }
-            low=i+1;
-            high=n-1;
-            int target = nums[i]*(-1);
-            while(low<high){
-                vector<int>v;
-                if((nums[low]+nums[high]) < target){
+            low = i + 1;
+            high = n - 1;
+            int target = 0 - nums[i];
+            while (low < high)
+            {
+
+                if ((nums[low] + nums[high]) < target)
+                {
                     low++;
                 }
-                else if((nums[low]+nums[high]) > target){
+                else if ((nums[low] + nums[high]) > target)
+                {
                     high--;
                 }
-                else{
+                else
+                {
+                    vector<int> v;
                     v.push_back(nums[i]);
                     v.push_back(nums[low]);
                     v.push_back(nums[high]);
                     ans.push_back(v);
-                    while((low+1) && nums[low] == nums[low+1]){
+                    while (low < high && nums[low] == nums[low + 1])
+                    {
                         low++;
                     }
-                    low++;
-                    while((high-1) && nums[high] == nums[high-1]){
+                    while (low < high && nums[high] == nums[high - 1])
+                    {
                         high--;
                     }
+                    low++;
                     high--;
                 }
             }
@@ -44,9 +53,8 @@ public:
         return ans;
     }
 };
-
-int main(){
-    
+int main()
+{
 
     return 0;
 }
