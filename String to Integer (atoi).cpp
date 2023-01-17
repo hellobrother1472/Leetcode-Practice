a6cdd6a922a7d5a1dcd7f34008101cd912a6e31e
hellobrother1472 <paras1472@gmail.com>
2023-01-17 13:58:55 +0530

    new

--- String to Integer (atoi).cpp
+++ String to Integer (atoi).cpp
@@ -129,6 +129,60 @@ public:
     }
 };
 
+// Reccursive approach
+// Approach is similar to above just make a string according to conditions and then remove initial zeros and then analyse and give answer.
+class Solution {
+public:
+    string ans = "";
+    bool num = false;
+    string removeInitZero(string a){
+        string sol = "";
+        bool number = false;
+        for(int i = 0 ; i<a.length() ; i++){
+            if(a[i] == '+' || a[i] == '-'){ sol += a[i] ;continue;}
+            if(a[i] >=49) number = true;
+            if(number) sol += a[i];
+        }
+        return sol;
+    }
+    void getStr(string s, int i){
+        if( 58<= s[i] || (s[i]<= 47 && s[i] != 45 && s[i] != 43 && s[i] != ' ') || i>=s.length()){
+            return;
+        }
+        if(num && (s[i] == 45 || s[i] == 43 || s[i] == ' ')){
+            return;
+        }
+
+        if(s[i] == ' ') {
+            getStr(s,i+1);
+        }
+        else{
+            ans += s[i];
+            num = true;
+            getStr(s,i+1);
+        }
+    }
+    int myAtoi(string s) {
+        if(s.length() == 0) return 0;
+        getStr(s,0);
+        ans = removeInitZero(ans);
+        if(ans.length() == 0 || ans[ans.length()-1] == '-' || ans[ans.length()-1] == '+') return 0;
+        if(ans.length()>= 12){
+            if(ans[0] == '-') return INT_MIN;
+            return INT_MAX;
+        }
+        long long longAns;
+        if(ans.length() >= 9){
+            longAns = stoll(ans);
+            if(longAns<= INT_MIN) return INT_MIN;
+            if(longAns>=INT_MAX) return INT_MAX;
+        }
+        int realAns = stoi(ans);
+        return realAns;
+        return 0;
+    }
+};
+
 int main()
 {
     return 0;
