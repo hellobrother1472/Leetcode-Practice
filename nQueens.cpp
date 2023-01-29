5e5d85f3b375a07494a8807d942d4b6561a93adf
hellobrother1472 <paras1472@gmail.com>
2023-01-29 13:55:13 +0530

    nqueens some comments added

--- nQueens.cpp
+++ nQueens.cpp
@@ -94,7 +94,7 @@ public:
 class Solution2 {
 public:
 
-    void solve(int col, vector<vector<string>> &ans, vector<string> &board, vector<int> &leftRow, vector<int>&upperDiagonal, vector<int> lowerDiagonal, int n){
+    void solve(int col, vector<vector<string>> &ans, vector<string> &board, vector<int> &rowMap, vector<int>&upperDiagonal, vector<int> lowerDiagonal, int n){
         if (col == n)
         {
             ans.push_back(board);
@@ -104,14 +104,14 @@ public:
         // Just normal adding value, doing reccursion and removing that value.
         for (int row = 0; row < n; row++)
         {
-            if(leftRow[row] == 0 && lowerDiagonal[row+col] == 0 && upperDiagonal[n-1+col-row] == 0){
+            if(rowMap[row] == 0 && lowerDiagonal[row+col] == 0 && upperDiagonal[n-1+col-row] == 0){
                 board[row][col] = 'Q';
-                leftRow[row] = 1;
+                rowMap[row] = 1;
                 lowerDiagonal[row+col] = 1;
                 upperDiagonal[n-1+col-row] = 1;
-                solve(col+1, ans,board,leftRow,upperDiagonal,lowerDiagonal,n);
+                solve(col+1, ans,board,rowMap,upperDiagonal,lowerDiagonal,n);
                 board[row][col] = '.';
-                leftRow[row] = 0;
+                rowMap[row] = 0;
                 lowerDiagonal[row+col] = 0;
                 upperDiagonal[n-1+col-row] = 0;  
             }
@@ -123,13 +123,13 @@ public:
         vector<vector<string>> ans;
         vector<string> board(n);
         string s(n, '.');
-        vector<int> leftRow(n,0) , upperDiagonal(2*n-1,0), lowerDiagonal(2*n-1,0); // we are using this to use as map as we place the queen and update the values so that we can check if the present box does not lie in the restricted region.
+        vector<int> rowMap(n,0) , upperDiagonal(2*n-1,0), lowerDiagonal(2*n-1,0); // we are using this to use as map as we place the queen and update the values so that we can check if the present box does not lie in the restricted region.
         for (int i = 0; i < n; i++)
         {
             board[i] = s;
         }
 
-        solve(0, ans, board,leftRow,upperDiagonal,lowerDiagonal, n);
+        solve(0, ans, board,rowMap,upperDiagonal,lowerDiagonal, n);
 
         return ans;
     }
