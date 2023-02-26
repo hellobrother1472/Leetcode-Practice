40d23a661dd6f092542fff4047bd462f7ea68509
hellobrother1472 <paras1472@gmail.com>
2023-02-26 12:23:13 +0530

    odd or even by bit manipulation

---
+++ Odd or Even by bit manipulation.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{   
+public:
+    string oddEven(int N){
+        if( N&1 != 0) return "odd";
+        return "even";
+    }
+};
+
+int main(){
+
+    return 0;
+}
