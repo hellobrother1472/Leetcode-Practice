797c2a4fa759036ca86a0cffccc31d56b5a32fe3
hellobrother1472 <paras1472@gmail.com>
2023-02-27 09:34:01 +0530

    implement queue using array

---
+++ Implement Queue using array.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+class MyQueue
+{
+    private:
+        int arr[100005];
+        int front;
+        int rear;
+
+    public:
+        MyQueue()
+        {
+            front = 0;
+            rear = 0;
+        }
+        void push(int);
+        int pop();
+};
+// Function to push an element x in a queue.
+void MyQueue ::push(int x)
+{
+    rear++;
+    arr[rear] = x;
+}
+
+// Function to pop an element from queue and return that element.
+int MyQueue ::pop()
+{
+    if (front == rear) return -1;
+    front++;
+    int store = arr[front];
+    return store;
+}
+
+int main()
+{
+
+    return 0;
+}
