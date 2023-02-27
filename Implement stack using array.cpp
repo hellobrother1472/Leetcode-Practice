524cf9048d0ae3ec79e84d982356233f28bbc229
hellobrother1472 <paras1472@gmail.com>
2023-02-27 09:34:13 +0530

    implement stack using array

---
+++ Implement stack using array.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Function to push an integer into the stack.
+
+class MyStack
+{
+private:
+    int arr[1000];
+    int top;
+public:
+    MyStack(){top=-1;}
+    int pop();
+    void push(int);
+};
+
+void MyStack :: push(int x)
+{
+    top++;
+    arr[top] = x;
+}
+
+//Function to remove an item from top of the stack.
+int MyStack :: pop()
+{
+    if(top == -1) return -1;
+    int store = arr[top];
+    top--;
+    return store;
+}
+
+int main(){
+
+    return 0;
+}
