e1a8978a3834b58a5c1d42f7fb56876ae29c6b3a
hellobrother1472 <paras1472@gmail.com>
2023-03-21 10:18:43 +0530

    sum of subarray ranges

---
+++ Sum of Subarray Ranges.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Brute Force : Generate all the subarray with taking two variables min and max and then get the answer.
+
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        
+    }
+};
+
+int main(){
+
+    return 0;
+}
