fcc04bbb791e4584aee64b61c2ea7238af0f4d34
hellobrother1472 <paras1472@gmail.com>
2023-03-28 11:55:47 +0530

    Kth largest element

---
+++ Kth Largest Element in an Array.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()-k];
+    }
+};
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        for(int i = 0; i < nums.size();i++){
+            minHeap.push(nums[i]);
+            if(minHeap.size() > k) minHeap.pop();
+        }
+        return minHeap.top();
+    }
+};
+
+int main(){
+
+    return 0;
+}
