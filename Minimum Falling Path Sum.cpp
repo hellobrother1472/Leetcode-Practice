665bd612f272a8ea30d56e32b7acdf3253cfa2c7
hellobrother1472 <paras1472@gmail.com>
2023-04-21 10:21:06 +0530

    MInimum falling path sum

--- Minimum Falling Path Sum.cpp
+++ Minimum Falling Path Sum.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// Reccursion (solution of 2022)
 class Solution {
 private:
     int f(int i,int j,int n,int m,vector<vector<int>>& a)
@@ -35,6 +36,96 @@ public:
         return ans;
     }
 };
+
+// Approach is that for current index the minimum value will be the mini (minimum of three below as per condition)+ current element.
+class Solution {
+private:
+    int helper(vector<vector<int>>& matrix,vector<vector<int>>& dp, int n, int i, int j){
+        if(i == n-1){
+            return matrix[i][j];
+        }
+        if(!dp[i][j]){
+            int mini = INT_MAX;
+            for(int k = -1 ; k <=1; k++){
+                if(j+k >= 0 && j+k < n) mini = min(mini, helper(matrix,dp,n,i+1,j+k));
+            }
+            dp[i][j] = matrix[i][j] + mini;
+        }
+        return dp[i][j];
+    }
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        int mini = INT_MAX;
+        for(int k = 0 ; k < n; k++){
+            mini = min(mini, helper(matrix,dp,n,0,k));
+        }
+        return mini;
+    }
+};
+
+// Tabulation
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int i = n-1 ; i >= 0; i--){
+            for(int j = n-1 ; j >= 0; j--){
+                if(i == n-1){
+                    dp[i][j] = matrix[i][j];
+                    continue;
+                }
+                int minimum = INT_MAX;
+                for(int k = -1 ; k <=1; k++){
+                    if(j+k >= 0 && j+k < n) minimum = min(minimum,dp[i+1][j+k]);
+                }
+                dp[i][j] = matrix[i][j] + minimum;
+            }
+        }
+
+        // Finding minimum from first row of dp
+        int ans = INT_MAX;
+        for(int i = 0; i < n; i++){
+            ans = min(ans,dp[0][i]);
+        }
+        return ans;
+    }
+};
+
+
+// Space optimization
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<int>prev(n,0);
+        for(int i = n-1 ; i >= 0; i--){
+            vector<int>curr(n,0);
+            for(int j = n-1 ; j >= 0; j--){
+                if(i == n-1){
+                    curr[j] = matrix[i][j];
+                    continue;
+                }
+                int minimum = INT_MAX;
+                for(int k = -1 ; k <=1; k++){
+                    if(j+k >= 0 && j+k < n) minimum = min(minimum,prev[j+k]);
+                }
+                curr[j] = matrix[i][j] + minimum;
+
+            }
+            prev = curr;
+        }
+        int ans = INT_MAX;
+        // Finding minimum from prev
+        for(int i = 0; i < n; i++){
+            ans = min(ans,prev[i]);
+        }
+        return ans;
+    }
+};
+
 
 int main(){
     
